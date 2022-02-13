fixing some problems on normalization

lib/generate_default.py
@@ -1,3 +1,4 @@
+from matplotlib.pyplot import axis
 from tqdm import tqdm
 
 import pickle
@@ -10,20 +11,20 @@ import gc
 
 from .normalization import normalize
 
-SPFN_FEATURES = {
+DEFAULT_FEATURES = {
     'plane': ['location', 'z_axis', 'normalized'],
     'cylinder': ['location', 'z_axis', 'radius', 'normalized'],
     'cone': ['location', 'z_axis', 'radius', 'angle', 'apex', 'normalized'],
     'sphere': ['location', 'radius', 'normalized']
 }
 
-def filterFeature2SPFN(feature, name):
+def filterFeature2DEFAULT(feature, name):
     tp = feature['type'].lower()
-    if tp in SPFN_FEATURES.keys():
+    if tp in DEFAULT_FEATURES.keys():
         feature_out = {}
         feature_out['name'] = name
         feature_out['type'] = tp
-        for field in SPFN_FEATURES[tp]:
+        for field in DEFAULT_FEATURES[tp]:
             if field == 'normalized':
                 feature_out[field] = True
             else:
@@ -34,7 +35,7 @@ def filterFeature2SPFN(feature, name):
     feature['type'] = tp
     return feature
 
-def generateH52SPFN(point_cloud, h5_filename, labels = None, features_data = None, norm_parameters = None):
+def generateH52DEFAULT(point_cloud, h5_filename, labels = None, features_data = None, norm_parameters = None):
     with h5py.File(h5_filename, 'w') as h5_file:
         noise_limit = 0.
         if 'add_noise' in norm_parameters.keys():
@@ -73,7 +74,6 @@ def generateH52SPFN(point_cloud, h5_filename, labels = None, features_data = Non
             if len(feature['point_indices']) > 0:
                 soup_name = f'{name}_soup_{i}'
                 grp = h5_file.create_group(soup_name)
-                gt_points = points[feature['point_indices']]
-                grp.create_dataset('gt_points', data=gt_points)
-                feature = filterFeature2SPFN(feature, soup_name)
+                grp.create_dataset('gt_indices', data=feature['point_indices'])
+                feature = filterFeature2DEFAULT(feature, soup_name)
                 grp.attrs['meta'] = np.void(pickle.dumps(feature))

lib/normalization.py
@@ -115,16 +115,19 @@ def normalize(point_cloud, parameters, features=[]):
     transforms = {
         'translation': np.zeros(3),
         'rotation': np.eye(3),
-        'scale': 0.
+        'scale': 0.,
+        'sequence': ['translation', 'rotation', 'scale']
     }
 
     if 'rescale' in parameters.keys():
-        points, features, transforms['scale'] = rescale(points, features, parameters['centralize'])
-    points, features, transforms['translation'] = centralize(points, features) if 'centralize' in parameters.keys() and parameters['centralize'] == True else points, features, None
-    points, normals, features, transforms['rotation'] = alignCanonical(points, normals, features) if 'align' in parameters.keys() and parameters['align'] == True else points, normals, features, None
+        points, features, transforms['scale'] = rescale(points, features, parameters['rescale'])
+    if 'centralize' in parameters.keys() and parameters['centralize'] == True:
+        points, features, transforms['translation'] = centralize(points, features)
+    if 'align' in parameters.keys() and parameters['align'] == True:
+        points, normals, features, transforms['rotation'] = alignCanonical(points, normals, features)
     if 'add_noise' in parameters.keys():
         points = addNoise(points, normals, parameters['add_noise'])
-    if 'cube_rescale' in parameters.keys():
+    if 'cube_rescale' in parameters.keys() and parameters['cube_rescale'] > 0:
         points, features, scale = cubeRescale(points, features, parameters['cube_rescale'])
         transforms['scale'] *= scale
 