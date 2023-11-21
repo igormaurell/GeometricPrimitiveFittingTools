fixing some errors in residual error computation

dataset_evaluator.py
@@ -11,7 +11,7 @@ from lib.utils import computeFeaturesPointIndices, writeColorPointCloudOBJ, getA
 from lib.matching import mergeQueryAndGTData
 from lib.evaluator import computeIoUs
 from lib.primitives import ResidualLoss
-from lib.normalization import rescale
+from lib.normalization import rescale, cubeRescale
 from pprint import pprint
 
 from asGeometryOCCWrapper.surfaces import SurfaceFactory
@@ -236,7 +236,6 @@ def process(data_tuple):
 
     instance_ious = []
     fpi_gt = None
-    gt_points = None
     gt_normals = None
     #type_ious = []
     if gt_data is not None:
@@ -249,10 +248,18 @@ def process(data_tuple):
         gt_points = gt_data['points']
         gt_normals = gt_data['normals']
     
+    reescale_factor = 1.
+    if cube_reescale_factor > 0:
+        if gt_points is not None:
+            _, _, reescale_factor = cubeRescale(gt_points.copy())
+        else:
+            _, _, reescale_factor = cubeRescale(points.copy())
+    
     if not no_use_occ_geometries:
         points, features, _ = rescale(points, features=features, factor=1000)
         if gt_data is not None:
-            gt_points, _, _ = rescale(gt_points, features=[], factor=1000)
+            gt_points, _, _ = rescale(gt_points, features=[], factor=1000)  
+        reescale_factor /= 1000
 
     for i, feature in enumerate(features):
         indices = fpi[i]
@@ -288,9 +295,8 @@ def process(data_tuple):
                 else:
                     distances = residual_distance.residual_loss(points_curr, feature)
                     angles = []
-                
-                if not no_use_occ_geometries:
-                    distances /= 1000.
+
+                distances *= reescale_factor
 
                 dataset_errors[tp]['distances'].append(distances)
                 dataset_errors[tp]['angles'].append(angles)
@@ -306,11 +312,8 @@ def process(data_tuple):
                     else:
                         distances_to_gt = residual_distance.residual_loss(points_gt_curr, feature)
                         angles_to_gt = []
-                    
-                    if not no_use_occ_geometries:
-                        distances_to_gt /= 1000.
-
-                    #print(len(distances_to_gt), np.max(points_gt_curr), feature['type'], feature['location'], np.mean(distances_to_gt))
+                
+                    distances_to_gt*= reescale_factor
 
                     dataset_errors[tp]['distances_to_gt'].append(distances_to_gt.astype(np.float32))
                     dataset_errors[tp]['angles_to_gt'].append(angles_to_gt)
@@ -396,6 +399,7 @@ if __name__ == '__main__':
     parser.add_argument('--no_use_occ_geometries', action='store_true')
     parser.add_argument('--ignore_primitives_orientation', action='store_true')
     parser.add_argument('-un', '--unnormalize', action='store_true', help='')
+    parser.add_argument('-crf', '--cube_reescale_factor', type=float, default = 0, help='')
 
     args = vars(parser.parse_args())
 
@@ -422,6 +426,7 @@ if __name__ == '__main__':
     no_use_occ_geometries = args['no_use_occ_geometries']
     ignore_primitives_orientation = args['ignore_primitives_orientation']
     unnormalize = args['unnormalize']
+    cube_reescale_factor = args['cube_reescale_factor']
 
     if gt_dataset_folder_name is not None or gt_data_folder_name is not None or gt_format is not None:
         if gt_data_folder_name is None:

dataset_merger.py
@@ -101,8 +101,7 @@ def addDictionaries(dict1, dict2):
     return result_dict
 
 def mergeByMax(counts, features):
-    ind = counts.index(max(counts))
-    return features[ind]
+    return features[0]
 
 def mergeByWeightedMean(counts, features):
     total_count = sum(counts)
@@ -131,16 +130,20 @@ def mergeByWeightedMean(counts, features):
                 else:
                     new_f[key] += value*weights[ind]
     return new_f
-    
+
+def maskValidFeatures(features):
+    return np.asarray([not ('invalid' in f and f['invalid'] == True) for f in features])
+
 def mergeFeatures(features, method='max'):
     new_features = [None for _ in range(len(features))]
     for i in range(len(features)):
         if features[i] is not None:
-            just_features = [x[0] for x in features[i]]
-            counts = [x[1] for x in features[i]]
+            features_curr = sorted(features[i], key=lambda x: x[1])[::-1]
+            just_features = [x[0] for x in features_curr]
+            counts = [x[1] for x in features_curr]
             types_count = {}
             types_indices = {}
-            for j in range(len(features[i])):
+            for j in range(len(features_curr)):
                 tp = just_features[j]['type']
                 if tp not in types_count:
                     types_count[tp] = counts[j]
@@ -160,12 +163,24 @@ def mergeFeatures(features, method='max'):
             just_features = [just_features[ind] for ind in final_indices]
             counts = [counts[ind] for ind in final_indices]
 
-            funcs = {
-                'max': mergeByMax,
-                'wm': mergeByWeightedMean
-            }
+            # special cases, no valid feature or just one valid
+            mask_valid = maskValidFeatures(just_features)
+            if np.count_nonzero(mask_valid) == 0:
+                new_f = just_features[0]
+            elif np.count_nonzero(mask_valid) == 1:
+                new_f = just_features[np.argmax(mask_valid)]
+            else:
+                # more than 2 valid features, removing the invalid ones
+                just_features = [just_features[ind] for ind in range(len(just_features)) if mask_valid[ind]]
+                counts = [counts[ind] for ind in range(len(counts)) if mask_valid[ind]]
 
-            new_f = funcs[method](counts, just_features)
+                funcs = {
+                    'max': mergeByMax,
+                    'wm': mergeByWeightedMean
+                }
+
+                new_f = funcs[method](counts, just_features)
+            
             new_features[i] = new_f
 
     return new_features
@@ -206,7 +221,8 @@ if __name__ == '__main__':
     parser.add_argument('--transform_folder_name', type=str, default = 'transform', help='transform folder name.')
     parser.add_argument('--merge_method', choices=['max', 'wm'], type=str, default = 'wm', help='')
 
-    parser.add_argument('--use_gt_transform', action='store_true', help='flag to use transforms from ground truth dataset (not needed if the dataset folder is the same)')
+    parser.add_argument('--use_input_gt_transform', action='store_true', help='flag to use transforms from ground truth dataset (not needed if the dataset folder is the same)')
+    parser.add_argument('--no_use_output_gt_transform', action='store_false', help='')
 
     parser.add_argument('--no_use_data_primitives', action='store_true')
 
@@ -235,7 +251,7 @@ if __name__ == '__main__':
     transform_folder_name = args['transform_folder_name']
     merge_method = args['merge_method']
 
-    use_gt_transform = args['use_gt_transform']
+    use_input_gt_transform = args['use_input_gt_transform']
 
     use_data_primitives = not args['no_use_data_primitives']
 
@@ -274,7 +290,7 @@ if __name__ == '__main__':
         input_gt_parameters[input_format]['transform_folder_name'] = input_gt_transform_format_folder_name
         input_gt_parameters[input_format]['unnormalize'] = True
 
-    if use_gt_transform and input_gt_transform_format_folder_name is not None:
+    if use_input_gt_transform and input_gt_transform_format_folder_name is not None:
         input_parameters[input_format]['transform_folder_name'] = input_gt_transform_format_folder_name
 
     output_parameters = {}

lib/fitting_func.py
@@ -103,7 +103,11 @@ class LeastSquares:
     def __init__(self):
         pass
 
-    def lstsq(self, A, Y, lamb=0.0):
+    def lstsq(self, A, Y, lamb=0.0, max_points=85000): #more than 85k points throw an error
+        if A.shape[0] > max_points:
+            shuffled_indices = torch.randperm(A.size(0))
+            A = A[shuffled_indices[:max_points]]
+            Y = Y[shuffled_indices[:max_points]]
         """
         Differentiable least square
         :param A: m x n
@@ -235,7 +239,6 @@ class FittingFunctions:
 
     @staticmethod
     def fit_sphere_torch(points, normals, weights, show_warning=False):
-
         N = weights.shape[0]
         sum_weights = torch.sum(weights) + EPS
         A = 2 * (- points + torch.sum(points * weights, 0) / sum_weights)
@@ -345,20 +348,6 @@ class FittingFunctions:
         'Cone': fit_cone_torch.__func__,
         'Sphere': fit_sphere_torch.__func__
     }
-
-    @staticmethod
-    def fit_by_global(primitive_type, points_full, normals_full, mask, weights=None):
-        points_full, _, scale = cubeRescale(points_full.copy())
-
-        points = points_full[mask]
-        normals = normals_full[mask]
-
-        feature = FittingFunctions.fit(primitive_type, points, normals, weights=weights, scale=1.0)
-        _, features, _ = rescale(points, features=[feature], factor=1./scale)
-        feature = features[0]
-
-        return feature
-
 
     @staticmethod
     def fit(primitive_type, points, normals, weights=None, scale=1.0):